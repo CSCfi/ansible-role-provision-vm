6f93616cc696df446cd91eb6f83c01c00e78082d
Risto Laurikainen <risto.laurikainen@csc.fi>
2014-10-06 17:10:18 +0300

    Added a new role for provisioning VMs
    Created a role provision_vm that is used to provision VMs using
    virt-install. It creates a kickstart file and a script for calling
    virt-install (these are based on templates) and calls the
    customized virt-install script. Creating of a VM is delegated to
    a hypervisor host specified in the host_vars of the virtual
    machine. For example, the hypervisor for pouta-devel2 is
    taito-cloud2.
    
    Created a playbook that calls the new role (provisioning.yml).
    
    Modified host_vars for pouta-devel1 and pouta-devel2 so that they
    can be provisioned using the new role.

---
+++ templates/virt-install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ ! -f /etc/libvirt/qemu/{{ inventory_hostname }}.xml ]; then
+
+virt-install \
+  --name={{ inventory_hostname }} \
+  --os-variant=rhel6 \
+  --vcpus={{ vcpus }} \
+  --ram={{ ram }} \
+{% for disk in disks %}
+  --disk path={{ disk.path }},cache=writethrough,device=disk,bus=virtio,size={{ disk.size }},format=raw \
+{% endfor %}
+{% for bridge in bridges %}
+  --network bridge={{ bridge }},model=virtio \
+{% endfor %}
+  --connect=qemu:///system \
+  --location={{ location }} \
+  --graphics=vnc \
+  --noautoconsole \
+  --initrd-inject={{ kickstart_tempdir }}/{{ inventory_hostname }}.ks \
+  --extra-args="ks=file:/{{ inventory_hostname }}.ks"
+fi
